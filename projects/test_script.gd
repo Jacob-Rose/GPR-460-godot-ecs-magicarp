6e6a883adea5c4dc8e5e58599967d58df1279806
Jacob Rose <jacob.rose@mymail.champlain.edu>
2020-09-17 09:15:57 -0400

    MouseImage working, but cannot figure out how to make it work in the jake-modules folder with shared settings

---
+++ projects/test_script.gd
@@ -0,0 +1,18 @@
+extends MouseImage
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	set_texture(load("res://icon.png"))
+	pass
+	#var s = Summator.new() # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
